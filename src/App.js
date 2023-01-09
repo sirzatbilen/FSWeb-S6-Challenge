61ef8e6429c27d9fc9723b6a0b5827b5c154537a
Put your Name in Here <your@email.here>
2023-01-09 11:44:48 +0300

    proje ornek2 okey

--- src/App.js
+++ src/App.js
@@ -56,6 +56,11 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const characters = data.map((item,index) =>
+  <Karakter key = {index} name ={item.name} birthYear = {item.birth_year}/>
+  );
+
+
 
   return (
     <div className="App">
